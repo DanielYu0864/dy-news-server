64449e6542c1a670623f54400dfe506bdd44df79
Daniel.Yu <adam741963@gmail.com>
2021-06-02 10:48:15 -0700

    ADD: weather api & apikey

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import NewsAPI from 'ts-newsapi';
 import dotenv from 'dotenv';
+import axios from 'axios';
 
 dotenv.config();
 
@@ -8,11 +9,13 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const dyNewsKey: string = process.env.DY_API as string;
+
 app.get('/', (req, res) => {
   res.send('home');
 });
 
-app.get('/newsapi/:country', async (req, res) => {
+app.get(`/newsapi/key=${dyNewsKey}/:country`, async (req, res) => {
   const country: any = req.params.country;
 
   const apiKey: string = process.env.NEWS_API as string;
@@ -25,9 +28,19 @@ app.get('/newsapi/:country', async (req, res) => {
     page: 1,
   });
 
-  const { articles } = topHeadlines;
+  // const { articles } = topHeadlines;
 
   res.send(topHeadlines);
+});
+
+app.get(`/weatherapi/key=${dyNewsKey}/:city`, async (req, res) => {
+  const city: any = req.params.city;
+  const apiKey: string = process.env.OPENWEATHERMAP_API as string;
+  const { data } = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+  );
+
+  res.send(data);
 });
 
 app.listen(PORT, () =>
