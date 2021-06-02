7b1df8805c24ab4ab53a4de7d1d337b3897d9671
Daniel.Yu <adam741963@gmail.com>
2021-06-02 15:23:15 -0700

    ADD: access header

--- build/server.js
+++ build/server.js
@@ -10,6 +10,12 @@ dotenv_1.default.config();
 const app = express_1.default();
 const PORT = process.env.PORT || 5000;
 const dyNewsKey = process.env.DY_API;
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 // main route
 app.get('/', (req, res) => {
     res.send('dy-news-server');
--- server.ts
+++ server.ts
@@ -10,6 +10,16 @@ const PORT = process.env.PORT || 5000;
 
 const dyNewsKey: string = process.env.DY_API as string;
 
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 // main route
 app.get('/', (req, res) => {
   res.send('dy-news-server');
