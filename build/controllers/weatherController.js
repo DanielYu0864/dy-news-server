626db5ab34458daad37b52b6e0bce933771eaff4
Daniel.Yu <adam741963@gmail.com>
2021-06-17 12:15:28 -0700

    BUILD: run build

--- build/controllers/weatherController.js
+++ build/controllers/weatherController.js
@@ -12,16 +12,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.openWeatherMapApi = void 0;
+exports.openWeatherMapApiCrd = exports.openWeatherMapApiCity = void 0;
 const axios_1 = __importDefault(require("axios"));
-// @desc    Get news from open weather map api
-// @route   GET /api/key={}/weather/:country
+// @desc    Get weather info from open weather map api by city name
+// @route   GET /api/key={}/weather/:country/:language
 // @access  Private
-const openWeatherMapApi = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const openWeatherMapApiCity = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const city = req.params.city;
     const apiKey = process.env.OPENWEATHERMAP_API;
+    const language = req.params.lang ? req.params.lang : 'en';
     try {
-        const { data } = yield axios_1.default.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+        const { data } = yield axios_1.default.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${language}&appid=${apiKey}`);
         yield res.json(data);
     }
     catch (error) {
@@ -33,4 +34,26 @@ const openWeatherMapApi = (req, res) => __awaiter(void 0, void 0, void 0, functi
         }
     }
 });
-exports.openWeatherMapApi = openWeatherMapApi;
+exports.openWeatherMapApiCity = openWeatherMapApiCity;
+// @desc    Get wehater info from open weather map api by coordinates
+// @route   GET /api/key={}/weather/crd/:lat/:lon/:language
+// @access  Private
+const openWeatherMapApiCrd = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const lat = req.params.lat;
+    const lon = req.params.lon;
+    const apiKey = process.env.OPENWEATHERMAP_API;
+    const language = req.params.lang ? req.params.lang : 'en';
+    try {
+        const { data } = yield axios_1.default.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=${language}&appid=${apiKey}`);
+        yield res.json(data);
+    }
+    catch (error) {
+        const err = error;
+        if (err.response) {
+            console.log(err.response.status);
+            console.log(err.response.data);
+            yield res.json(err.response.data);
+        }
+    }
+});
+exports.openWeatherMapApiCrd = openWeatherMapApiCrd;
